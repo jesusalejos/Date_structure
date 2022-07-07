ff96912b39331c6b39decb89bc401ddfb2dc083f
Jesús Alejos <jalejos6@gmail.com>
2022-07-06 22:17:07 -0430

    The three first dataStructure: array, hashTable and linkList

---
+++ hashtable.js
@@ -0,0 +1,45 @@
+class HashTable {
+    constructor(size) {
+      this.data = new Array(size);
+    }
+    hashMethod(key) {
+      let hash = 0;
+      for (let i = 0; i < key.length; i++) {
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+      }
+      return hash;
+    }
+    
+    set(key, value) {
+      const address = this.hashMethod(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+          for (let i = 0; i < currentBucket.length; i++) {
+            if (currentBucket[i][0] === key) {
+              return currentBucket[i][1];
+            }
+          }
+        }
+        return undefined;
+      }
+  }
+  
+  const myHashTable = new HashTable(50);
+
+  console.log(myHashTable);
+  myHashTable.set("jesús",1987);
+  console.log(myHashTable);
+  myHashTable.set("elsy",1961);
+  console.log(myHashTable);
+  myHashTable.set("genesis",2008);
+  console.log(myHashTable);
+  console.log(myHashTable.get("genesis"))
