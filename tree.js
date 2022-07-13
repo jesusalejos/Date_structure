044e6f2d12eb71e8fcc2454f5236ed55e2b40a9b
Jesús Alejos <jalejos6@gmail.com>
2022-07-12 23:25:13 -0430

    Trees and graphs_2

---
+++ tree.js
@@ -0,0 +1,53 @@
+//      10
+//   4     20
+// 2  8  17  170
+
+class Node {
+    constructor(value) {
+      this.left = null;
+      this.right = null;
+      this.value = value;
+    }
+  }
+  
+  class BinarySearchTree {
+    constructor() {
+      this.root = null;
+    }
+    insert(value) {
+      const newNode = new Node(value);
+      if (this.root === null) {
+        this.root = newNode;
+      } else {
+        let currentNode = this.root;
+        while (true) {
+          if (value < currentNode.value) {
+            if (!currentNode.left) {
+              currentNode.left = newNode;
+              return this;
+            }
+            currentNode = currentNode.left;
+          } else {
+            if (!currentNode.right) {
+              currentNode.right = newNode;
+              return this;
+            }
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+  
+  const tree = new BinarySearchTree();
+
+  tree.insert(10);
+  console.log(tree);
+  tree.insert(4);
+  tree.insert(20);
+  console.log(tree);
+  tree.insert(2);
+  tree.insert(8);
+  tree.insert(17);
+  tree.insert(170);
+  console.log(tree);
